cc6609181d6bfccaa05a055b574c04a942d0b0c1
Jacob Ressler <jressler96@gmail.com>
2024-02-27 18:27:56 -0500

    topbar html is now dynamically injected

---
+++ js/topbar.js
@@ -0,0 +1,18 @@
+createTopBar = function() {
+    $('#top-bar').html(`
+    <div class="nav-left">
+            <div id="hamburger" class="nav-item" onclick="toggleSidebar()">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+            <a id="logo" class="nav-item" href="/">
+                <img src="/img/pmm_logo_2.png" alt="HOME">
+                <div>PMM</div>
+            </a>
+        </div>
+        <div class="nav-middle"></div>
+        <div class="nav-right">
+            <a href="/brackets" class="nav-item">BRACKETS</a>
+            <a href="/videos" class="nav-item">VIDEOS</a>
+        </div>`
+    );
+}
